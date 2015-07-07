63d27b978c8b3a7ae963859cdb04228037fb9002
Claire Mitchell <claireinezmitchell@gmail.com>
2015-07-07 16:18:43 +0100

    babysteps stuff done

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,20 +1,131 @@
+"use strict";
+
 var React = require("react");
 
-React.render(
-	<div>good morning</div>,
-	document.getElementById("content")
-);
+// var UserName = React.createClass({
+// 	render: function() {
+// 		var name = "anon";
+// 		return (
+// 			<h1>{this.props.name}</h1>
+// 		);
+// 	}
+// });
+
+// var UserStatus = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<p>{this.props.content}</p>
+// 		);
+// 	}
+// });
+
+// var LikeButton = React.createClass({
+// 	getDefaultProps: function() {
+// 		return {
+// 			liked: false
+// 		};
+// 	},
+// 	render: function() {
+// 		var buttonClass = "like";
+
+// 		if (this.props.liked) {
+// 			buttonClass += " yup";
+// 		}
+
+// 		return (
+// 			<button className={buttonClass}>
+// 				S
+// 			</button>
+// 		);
+// 	}
+// });
+
+// // React.render(<UserName />, document.getElementById("content"));
+
+
+// var User = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div className="app">
+// 				<UserName name="anon" />
+// 				<LikeButton liked={true} />
+// 				<UserStatus content="something about the election" />
+// 			</div>
+// 		);
+// 	}
+// });
+
+
+
+
+// React.render(<User />, document.getElementById("content"));
+
+
+// var React = require("react");
+// var PropTypes = React.PropTypes;
+// var rootEl = document.getElementById("content");
+
+// var GiveMeAName = React.createClass({
+
+// 	propTypes: {
+// 		name: PropTypes.string
+// 	},
+
+// 	render: function () {
+// 		return <h1>{this.props.name}</h1>;
+// 	}
+// });
+
+// var IHaveAName = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<GiveMeAName name="Claire" />
+// 		);
+// 	}
+// });
+
+// React.render(
+// 	<IHaveAName />,
+// 	rootEl
+// );
+
+
+// var LikeButton = React.createClass({
+// 	getInitialState: function() {
+// 		return {
+// 			liked: false
+// 		};
+// 	},
+// 	clickHandler: function(e) {
+// 		this.setState({
+// 			liked: !this.state.liked
+// 		});
+// 	},
+// 	render: function() {
+// 		var buttonClass = "like";
+
+// 		if (this.state.liked) {
+// 			buttonClass += " yup";
+// 		}
+
+// 		return (
+// 			<button className={buttonClass} onClick={this.clickHandler}>
+// 				{this.state.liked ? "-" : "+"}
+// 			</button>
+// 		);
+// 	}
+// });
+
+// React.render(<LikeButton />, document.getElementById("content"));
 
 
 var UserName = React.createClass({
 	render: function() {
-		var name = "anon";
 		return (
 			<h1>{this.props.name}</h1>
 		);
 	}
 });
-
 
 var UserStatus = React.createClass({
 	render: function() {
@@ -24,20 +135,16 @@ var UserStatus = React.createClass({
 	}
 });
 
-
 var LikeButton = React.createClass({
-	getInitialState: function() {
-		return {
-			liked: false
-		};
-	},
+
 	clickHandler: function(e) {
 		this.props.toggleLike();
 	},
+
 	render: function() {
 		var buttonClass = "like";
 
-		if (this.state.liked) {
+		if (this.props.liked) {
 			buttonClass += " yup";
 		}
 
@@ -49,30 +156,28 @@ var LikeButton = React.createClass({
 	}
 });
 
-
 var User = React.createClass({
-	getInitialState: function() {
-		return {
-			liked: false
-		};
-	},
-	toggleLike: function() {
-		this.setState({
-			liked: !this.state.liked
-		});
-	},
-	render: function() {
-		return (
-			<div className="app">
-				<UserName name="anon" />
-				<LikeButton liked={this.state.liked} toggleLike={this.toggleLike} />
-				<UserStatus liked={this.state.liked} content="something about the election" />
-			</div>
-		);
-	}
+  getInitialState: function() {
+    return {
+      liked: false
+    };
+  },
+
+  toggleLike: function() {
+    this.setState({
+      liked: !this.state.liked
+    });
+  },
+
+  render: function() {
+    return (
+      <div className="app">
+        <UserName name="anon" />
+        <LikeButton liked={this.state.liked} toggleLike={this.toggleLike} />
+        <UserStatus liked={this.state.liked} content="something about the election" />
+      </div>
+    );
+  }
 });
-
-
-
 
 React.render(<User />, document.getElementById("content"));
